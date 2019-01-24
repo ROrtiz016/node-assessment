c574a5102ea9e1fb9d64480c985e3af18e4cf952
ROrtiz016 <37273928+ROrtiz016@users.noreply.github.com>
2019-01-24 00:04:09 -0700

    completed

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const port = 3000
 app.use(bodyParser.json())
 
 app.get('/api/user',usersCtrl.getUsers)
+app.get('/api/user/:userId', usersCtrl.getUserById)
+app.get('/api/admin', usersCtrl.getAdmins)
+app.get('/api/nonadmin', usersCtrl.getNonAdmin)
+app.get('/api/type/:userType', usersCtrl.getUserType)
+app.put('/api/user/:userId', usersCtrl.updateUser)
+app.post('/api/user', usersCtrl.addUser)
+app.delete('/api/user/:userId', usersCtrl.removeUser)
 
 app.listen(port, () => {
   console.log(`Server online at port: ${port}`)
--- usersCtrl.js
+++ usersCtrl.js
@@ -2,15 +2,108 @@ const users = require('./userData')
 
 module.exports = {
   getUsers: (req, res, next) => {
+    results = users
     if(req.query.age){
-    users = users.filter((el)=> {
+    results = results.filter((el)=> {
       return el.age < req.query.age
     })
   } 
     if(req.query.email){
-      users = users.filter((el) => {
+      results = results.filter((el) => {
         return el.email === req.query.email
       })
       }
+    if(req.query.favorites){
+      results = results.filter((el) => {
+        return el.favorites.includes(req.query.favorites)
+      })
     }
+    res.status(200).send(results)
+  },
+
+  getUserById: (req, res, next) => {
+    var user;
+    for (var i = 0; i < users.length; i++){
+      if(req.params.userId <= users.length){
+        user = users.filter((el) => {
+          return el.id == req.params.userId;
+        })
+        res.status(200).send(user[0])
+      }else{
+        res.sendStatus(404)
+      }
+    }
+  },
+
+  getAdmins: (req, res, next) => {
+    var admins = users;
+    for(var i = 0; i < users.length; i++){
+      if(users[i].type){
+        admins = admins.filter((el) => {
+          return el.type === 'admin'
+        })
+        res.status(200).send(admins)
+      }
+    }
+  },
+
+  getNonAdmin: (req, res, next) => {
+    var nonAdmins = users;
+    for(var i = 0; i < users.length; i++){
+      if(users[i].type){
+        nonAdmins = nonAdmins.filter((el) => {
+          return el.type !== 'admin'
+        })
+        res.status(200).send(nonAdmins)
+      }
+    }
+  },
+
+  getUserType: (req, res, next) => {
+    var userType = users
+    for(var i = 0; i < users.length; i++){
+      if(users[i].type === req.params.userType){
+        userType = userType.filter((el) => {
+          return el.type === req.params.userType;
+        })
+        res.status(200).send(userType)
+      }
+    }
+  }, 
+
+  updateUser: (req, res, next) => {
+    const {id, first_name, last_name, type} = req.body
+    for( var i = 0; i < users.length; i++){
+      if(users[i].id == req.params.userId){
+          users[i].id = id,
+          users[i].first_name = first_name,
+          users[i].last_name = last_name,
+          users[i].type = type
+      }
+    }
+    res.status(200).send(users)
+  },
+
+  addUser: (req, res, next) => {
+    const {first_name, last_name, email, gender, language, age, city, state, type, favorites} = req.body
+    users.push({
+      id: users.length + 1,
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      gender: gender,
+      language: language,
+      age: age,
+      city: city,
+      state: state,
+      type: type,
+      favorites: favorites
+    })
+    res.status(200).send(users)
+  },
+
+  removeUser: (req, res, next) => {
+    users.splice(req.params.userId - 1, 1)
+    res.status(200).send(users)
   }
+}
